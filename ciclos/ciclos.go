ace21743dab310c79c15e4d0274dc07bf9e2296d
Alfredss <alfredss.24.7@gmail.com>
2016-11-03 22:09:15 -0500

    practicando con estructuras repetitivas

---
+++ ciclos/ciclos.go
@@ -0,0 +1,37 @@
+package main 
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	//FOR
+	for i:=0; i<10; i++ {
+		number_str := strconv.Itoa(i)
+		fmt.Printf("Number %v \n", number_str)
+	}
+
+	//simulando WHILE
+
+	j:=0
+
+	for j<10{
+		fmt.Println(j)
+		j++
+	}
+
+	//ciclos infinitos uso de BREAK y CONTINUE
+	k:=0
+	for {
+		if k == 2 {
+			k++
+			continue
+		}
+		fmt.Println(k)
+		if k>10 {
+			break
+		}	
+		k++
+	}
+}
