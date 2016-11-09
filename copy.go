c3848ebac181bdb4746a5a421d8cdd609b67471b
Alfredss <alfredss.24.7@gmail.com>
2016-11-08 19:44:58 -0500

    Trabajamos con punteros (pointers)

--- copy.go
+++ copy.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	slice := []int{1,2,3,4}
-	copia := make([]int, len(slice), cap(slice)*2)
+	copia := make([]int, len(slice), cap(slice)*2) //duplicamos la capacidad para que se cree un buen slice
 
 	copy(copia, slice) //copy(destino, fuente)s
-	fmt.Println(cap(copia))
+	fmt.Println(cap(copia)) 
 	fmt.Println(slice)
 	fmt.Println(copia)
 
---
+++ pointers.go
@@ -0,0 +1,24 @@
+package main 
+
+import(
+	"fmt"	
+)
+
+func main() {
+	var x, y *int //declaramos los punteros que almacenan direcciones
+	entero:=5//almacenamos 5 en la variable entero
+
+	x = &entero//almacenamos la direccion de entero en el puntero x
+	y = &entero
+
+	*x = 7 //accedemos al valor del puntero x con * y le asignamos el nuevo valor 6
+
+		   //como cambiamos el valor del puntero x entonces y y entero tambien se ven
+			//afectadas al apuntar estas a la misma direccion
+	fmt.Println(x)
+	fmt.Println(*x)
+	fmt.Println(y)
+	fmt.Println(*y)
+	fmt.Println(entero)
+
+}
