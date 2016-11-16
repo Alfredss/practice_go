e8758158bc456878d1f27a8e314ee84ddc1439b2
Alfredss <alfredss.24.7@gmail.com>
2016-11-15 19:52:01 -0500

    Creamos los metodos a nuestra estructura Usuario

--- structs.go
+++ structs.go
@@ -1,30 +1,51 @@
-package main 
+package main
 
-import(
+import (
 	"fmt"
 )
 
-type Usuario struct{
-	nombre string
+type Usuario struct {
+	nombre    string
 	apellidos string
-	edad int
+	edad      int
 }
 
-type Animal struct{
+type Animal struct {
 	nombre string
-	edad int
+	edad   int
+}
+
+//METODO en una FUNCION
+func (users Usuario) nombre_completo() string {
+	return users.nombre + " " + users.apellidos
+}
+
+func (this *Usuario) set_name(name string) {
+	this.nombre = name
 }
 
 func main() {
-	persona01 := Usuario{nombre: "alfredo", apellidos: "yupanqui", edad : 50}
-	animal := Animal{nombre:"gato", edad:2}
+
+	persona01 := Usuario{nombre: "alfredo", apellidos: "yupanqui", edad: 50}
+	animal := Animal{nombre: "gato", edad: 2}
 	animal2 := Animal{"perro", 5}
 
 	fmt.Println(persona01)
 	fmt.Println(animal)
 	fmt.Println(animal2)
 
-	animal3 := new(Animal) //devuelve el puntero del struc
+	fmt.Println(animal.nombre) //acceder a un atributo
 
+	animal3 := new(Animal) //devuelve el puntero del struc
+	fmt.Println(animal3)
 	//podemos acceder y asignar con las siguiente sintaxis: animal3.nombre = "leon"
+
+	//Metodo
+	coder := new(Usuario)
+	coder.nombre = "Alfredo"
+	coder.apellidos = "Yupanqui"
+
+	coder.set_name("Rafael")
+
+	fmt.Println(coder.nombre_completo())
 }
